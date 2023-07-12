0a90e38eb42a475edcfc64ba62b1b8a6b4ad1b11
lh555 <125605849+Zite0@users.noreply.github.com>
2023-07-12 13:57:56 -0400

    Script and instruction changes
    I added new files for the upcoming instructions of the experiment. I also modified the norming script with new data and added other necessary files.

--- data_includes/norming.js
+++ data_includes/norming.js
@@ -1,16 +1,56 @@
 PennController.ResetPrefix(null);
 
-newTrial("welcome-message");
+Sequence("init-recorder","welcome-message",randomize("reaction-time-exp"),"upload")
 
-newTrial("welcome",
+InitiateRecorder("TODO: SERVER-URL-HERE").label("init-recorder");
+
+newTrial("welcome-message",
 	newHtml("welcome-message","welcome-message.html")
 	.print()
     ,
-    newButton("begin","Click to start the experiment.")
+    newButton("begin","Click here to go over the instructions.")
     .center()
     .print()
     .wait()
 )
 
+// Instructions should go here. Will use template to show html files with the instructions. 
+// use buttons to continue to the next page.
 
+// TODO: figure out a way to have a recording trial run in the instructions.
+// maybe use conditional statements ?
+
+
+
+Template("items.csv", row =>
+    newTrial("reaction-time-exp"
+        ,
+        newImage("verb-image",row.image)
+            .center()
+            .size(120,120)
+            .print()
+        ,
+		newVar("RT").global().set(v=>Date.now())
+		,
+		// add media recorder
+		// how to determine onset of speech ??
+		newMediaRecorder("recorder","audio")
+			.record()
+		,
+		// record for 1.5 seconds immediately after image is printed on screen.
+		newTimer("recording",1500)
+			.start()
+			.wait()
+		,
+		getMediaRecorder("recorder")
+			.stop()
+		,
+		// Calculate reaction time with variable
+		getVar("RT").set( v => Date.now() - v )
+     
+	).log("ReactionTime",getVar("RT"))
+)
+
+// Maybe try to upload recording after each image ? 
+UploadRecordings("upload")
 
